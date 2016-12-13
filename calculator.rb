778eaf8f8d5d3a21bf5d2c54b40fe8e1ad36849c
Ian Lawson <IanLawsonDevelopment@gmail.com>
2016-12-13 15:21:26 -0500

    updated files

--- calculator.rb
+++ calculator.rb
@@ -1,20 +1,19 @@
-# ask the user for two numbers
-# ask the user for a n operation to perform
-# perform the operation on the the two numbers
-# outut the results
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
 
-Kernel.puts("Welcome to Calculator")
+prompt("Welcome to Calculator")
 
-Kernel.puts("What's the first number going to be?")
+prompt("What's the first number going to be?")
 first_num = Kernel.gets().chomp()
 
-Kernel.puts("What's the second number going to be?")
+prompt("What's the second number going to be?")
 second_num = Kernel.gets().chomp()
 
 # Kernel.puts("the first number is: " + first_num + "!")
 # Kernel.puts("the second number is: " + second_num + "!")
 
-Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+prompt("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
 operator = Kernel.gets().chomp()
 
 if operator == '1'
@@ -27,4 +26,4 @@ elsif operator == '4'
   result = first_num.to_f() / second_num.to_f()
 end
 
-Kernel.puts("The result is #{result}")
+prompt("The result is #{result}")
---
+++ pseudo_code.rb
@@ -0,0 +1,41 @@
+#Pseudo Code exercises
+
+#method that returns the sum of two integers
+
+START
+
+SET integer1
+SET integer2
+
+result = integer1 + integer2
+
+PRINT result
+
+END
+
+#method that takes an array of strings and 
+#returns a string that is all strings concatenated together
+
+START
+
+# Given array of strings
+
+Turn array of strings into one string and add spaces between values
+READ values of original array and add them together / Add space to end of each value
+
+PRINT new array
+
+END
+
+#method that takes an array of integers and returns a new array with every other element
+
+START
+
+#given array of integers
+
+READ given array and convert to float. 
+SET value of converstion to array_float
+
+Repeat for conversion to string
+
+END
